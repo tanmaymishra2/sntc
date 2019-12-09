d78ccbe6d28ac2ff85a818de39b879e718c6b52e
Dell <tanmaymishra2@gmail.com>
2019-12-10 03:45:46 +0530

    Pushing notifications through firebase

--- app/src/main/java/com/iitbhu/sntc/MainActivity.java
+++ app/src/main/java/com/iitbhu/sntc/MainActivity.java
@@ -1,14 +1,20 @@
 package com.iitbhu.sntc;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends Activity {
 
@@ -28,6 +34,31 @@ public class MainActivity extends Activity {
         webView.setWebViewClient(webViewClient);
 
         webView.loadUrl("https://www.sntciitbhu.co.in/");
+
+        //Generating notifications here
+
+        //For oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel=new NotificationChannel("SNTCNotifications", "SNTCNotifications",
+                    NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Successful";
+                        if (!task.isSuccessful()) {
+                            msg = "failed";
+                        }
+                        //Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 
 
@@ -40,7 +71,5 @@ public class MainActivity extends Activity {
 
         return super.onKeyDown(keyCode, event);
     }
-
-
 
 }
